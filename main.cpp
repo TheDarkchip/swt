aea342f7653c4f8ac8c9a410e27ecd406b4b7c95
Luk97 <lukas_nickel@gmx.de>
2021-01-17 16:01:43 +0100

    Comments

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
     time_t timetoday;
     time (&timetoday);
+	//Hochkomplizierte Funktionalität
     std::cout << "Hello World!";
     std::cout << "Calendar date and time as per todays is : "<< asctime(localtime(&timetoday));
     return 0;
