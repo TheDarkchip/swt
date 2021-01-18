6597e7b65d12e6e22793197b12de4d798d1455f4
Robert Pracht <Robi.Pracht@googlemail.com>
2021-01-18 15:51:11 +0100

    2 von 3 Zeilen

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main() {
     time_t timetoday;
     time (&timetoday);
 	//Hochkomplizierte Funktionalität
-    std::cout << "Hello World!";
+    std::cout << "Goodbye World!";
     std::cout << "Calendar date and time as per todays is : "<< asctime(localtime(&timetoday));
+    std::cout << "Quick Math";
     return 0;
 }
