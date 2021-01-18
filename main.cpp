72be2a3c7f766263ac1e8a304e187913841389b8
Thanh <Tenschgo3@web.de>
2021-01-18 16:39:46 +0100

    Änderung sein Vater

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main() {
     time_t timetoday;
     time (&timetoday);
 	//Hochkomplizierte Funktionalität
-    std::cout << "Goodbye World!";
+    std::cout << "Goodbye lovely  World!";
     std::cout << "Calendar date and time as per todays is : "<< asctime(localtime(&timetoday));
-    std::cout << "Quick Math";
+    std::cout << 3 + 3 + 1
+    std::cout << "Quick Math's father";
     return 0;
 }
